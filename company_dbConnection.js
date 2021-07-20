82905cbbad1d6632d535177d56d93ed043aa2ec4
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-19 20:00:41 -0400

    Added mysql connection with host, port, user pass, and preferred db connection

---
+++ company_dbConnection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port, if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: '@That1Guy7',
+  database: 'playlistDB',
+});
