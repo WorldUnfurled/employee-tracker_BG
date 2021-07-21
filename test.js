80877274bf81f87c73e24e17fe570c548100aaa3
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-07-20 20:50:17 -0400

    Created 3 working SQL variables for department: view selects all from department; addDepartment inserts new values into department; updateDepartment updates a department row

--- test.js
+++ test.js
@@ -8,8 +8,48 @@ const connection = mysql.createConnection({
   database: 'company_db'
 });
 
+const id = "id";
+const name = "name";
+
+const updateDepartment = () => {
+    connection.query('UPDATE department SET ? WHERE ?',
+            [
+              {
+                id: 111
+              },
+              {
+                id: 1
+              },
+            ], (err) => {
+                if (err) throw err;
+              }
+    );
+}
+
+const addDepartment = () => {
+    connection.query(`INSERT INTO department(\`${id}\`, \`${name}\`) VALUES (121, 'Purchasing')`, (err, res) => {
+        if (err) throw err;
+    });
+} 
+
+const viewDepartment = () => {
+    connection.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err;
+        console.log(res);
+    });
+}
+
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected with id ~ ${connection.threadId} ~`);
-    init();
+    // addDepartment(); // Works
+    viewDepartment(); // Works
+    // viewRole();
+    // viewEmployee();
+    // addRole();
+    // addEmployee();
+    // updateDepartment(); // Works
+    // updateRole();
+    // updateEmployee();
+    connection.end();
 });
