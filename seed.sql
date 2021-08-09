1ee52f96fbfd925387bd0d00a60906c5cae38266
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-09 15:59:12 -0400

    Refactored sql seed data to auto incremenet ids and disallow null values for them

--- seed.sql
+++ seed.sql
@@ -1,25 +1,25 @@
-IF DATABASE company_db EXISTS DROP;
+DROP company_db IF EXISTS;
 CREATE DATABASE company_db;
 
 USE company_db;
 
 CREATE TABLE `department` (
-	`id` INT PRIMARY KEY,
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(30)
 );
 
 CREATE TABLE `role` (
-	`id` INT PRIMARY KEY,
+	`id` INT NOT NULLPRIMARY KEY AUTO_INCREMENT,
     `title` VARCHAR(30),
     `salary` DECIMAL,
     `department_id` INT
 );
 
 CREATE TABLE `employee` (
-	`id` INT PRIMARY KEY,
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `first_name` VARCHAR(30),
     `last_name` VARCHAR(30),
-    `role_id` INT PRIMARY KEY,
+    `role_id` INT,
     `manager_id` INT
 );
 
--- test.js
+++ test.js
@@ -8,73 +8,57 @@ const connection = mysql.createConnection({
   database: 'company_db'
 });
 
-const conQuery = (query) => {
-    connection.query(query, (err, res) => {
-        if (err) throw err;
-    });
-}
-const conQueryRows = (query, rows) => {
+//
+const conQuery = (query, rows) => {
     connection.query(query, rows, (err, res) => {
         if (err) throw err;
-    });
-}
-
-const conQueryWhere = (query, where) => {
-    connection.query(query, where, (err, res) => {
-        if (err) throw err;
+        console.table(res);
     });
 }
 
 // ADD
-// 1. DEPARTMENT
 const addDepartment = (id, name) => {
     let query = "INSERT INTO department(id, name) VALUES (?, ?)"; 
     let rows = [id, name];
-    conQueryRows(query, rows);
+    conQuery(query, rows);
 }
 
-const viewAll = (table) => {
-    let query = "SELECT * FROM ?";
-    conQuery(query, table);
-}
-
-// 2. ROLE
-const addRole = (id, title, salary, dept_id) => { // ADD ROLE
-    let query = "INSERT INTO role(id, title, salary, department_id) VALUES (?, ?, ?, ?)";
-    let rows = [ id, title, salary, dept_id ];
-    conQueryRows(query, rows);
-}
-const viewRolesByDepartment = (dept_id) => { // VIEW ALL ROLES BY DEPARTMENT
-    let query = "SELECT * FROM role WHERE ?";
-    let where = [ { dept_id: 111 } ];
-    conQueryWhere(query, where);
-}
-
-// 3. EMPLOYEE
 const addEmployee = (id, first_name, last_name, role_id, manager_id) => {
     let query = "INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?, ?)";
     let rows = [ id, first_name, last_name, role_id, manager_id ];
-    conQueryRows(query, rows);
+    conQuery(query, rows);
 }
 
+const addRole = (id, title, salary, dept_id) => { // ADD ROLE
+    let query = "INSERT INTO role(id, title, salary, department_id) VALUES (?, ?, ?, ?)";
+    let rows = [ id, title, salary, dept_id ];
+    conQuery(query, rows);
+}
+
+// VIEW
+const viewAll = (table) => {
+    let query = "SELECT * FROM " + table;
+    conQuery(query);
+}
+
+const viewByDepartment = (table, dept_id) => { // VIEW ALL ROLES BY DEPARTMENT
+    let query = "SELECT * FROM ? WHERE ?";
+    let where = [ { dept_id: dept_id } ];
+    conQuery(query, table, where);
+}
+
+// Update
 const updateEmployee = (updateKey, oldValue, updateValue) => {
     let query = "UPDATE department SET ? WHERE ?";
     let where = [ { [updateKey]: oldValue }, { [updateKey]: updateValue } ];
-    conQueryWhere(query, where);
+    conQuery(query, where);
 }
 
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected with id ~ ${connection.threadId} ~`);
-    addDepartment("14", "Sanitization");
-    // addDepartment(); // Works
-    // viewDepartment(); // Works
-    // viewRole();
-    // viewEmployee();
-    // addRole();
-    // addEmployee();
-    // updateDepartment(); // Works
-    // updateRole();
-    // updateEmployee();
+    viewAll("department");
+    addEmployee(317, "Larry", "Schumer", 14, 145);
+    viewAll("employee");
     connection.end();
 });
