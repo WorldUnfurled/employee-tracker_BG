da2bedc98789deaf0e624e3575c26d187c7395a7
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-08-09 22:27:31 -0400

    Changed naming conventions in add___Questions to match SQL function params; added function call pseudocode (commented out) in each add___Prompt

--- inquirer.js
+++ inquirer.js
@@ -30,7 +30,7 @@ const addQuestions = [
 const addDepartmentQuestions = [
     {
         type: 'input',
-        name: 'addDepartmentName',
+        name: 'name',
         message: 'What is the department\'s name?'
     }
 ]
@@ -38,17 +38,17 @@ const addDepartmentQuestions = [
 const addRoleQuestions = [
     {
         type: 'input',
-        name: 'addRoleTitle',
+        name: 'title',
         message: 'What is the role\'s title?'
     },
     {
         type: 'input',
-        name: 'addRoleTitle',
+        name: 'salary',
         message: 'What is the salary for this role?'
     },
     {
         type: 'list',
-        name: 'addRoleDeptId',
+        name: 'dept_id',
         message: 'What is the id of the department this role is under?'
     }
 ]
@@ -56,22 +56,22 @@ const addRoleQuestions = [
 const addEmployeeQuestions = [
     {
         type: 'input',
-        name: 'addEmployeeFirst',
+        name: 'first_name',
         message: 'What is the employee\'s first name?'
     },
     {
         type: 'input',
-        name: 'addEmployeeLast',
+        name: 'last_name',
         message: 'What is the employee\'s last name?'
     },
     {
         type: 'list',
-        name: 'addEmployeeRoleId',
+        name: 'role_id',
         message: 'What is the ID of the role the employee operates under?'
     },
     {
         type: 'list',
-        name: 'addEmployeeManagerId',
+        name: 'manager_id',
         message: 'What is the ID of the manager the employee operates under?'
     }
 ]
@@ -151,19 +151,25 @@ const addPrompt = () => {
 const addDepartmentPrompt = () => {
     inquirer
     .prompt(addDepartmentQuestions)
-    .then(data => {});
+    .then(data => {
+        // addDepartment(data.name);
+    });
 }
 
 const addRolePrompt = () => {
     inquirer
     .prompt(addRoleQuestions)
-    .then(data => {});
+    .then(data => {
+        // addRole(data.title, data.salary, data.dept_id);
+    });
 }
 
 const addEmployeePrompt = () => {
     inquirer
     .prompt(addEmployeeQuestions)
-    .then(data => {});
+    .then(data => {
+        // addEmployee(data.first_name, data.last_name, data.role_id, data.manager_id);
+    });
 }
 
 const updatePrompt = () => {
